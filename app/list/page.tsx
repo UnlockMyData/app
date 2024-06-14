7debfdcc66f618f75855539cca107657443809f2
Tonyac-create <angelique.copere@gmail.com>
2024-06-14 13:53:19 +0200

    first commit test

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -3,9 +3,9 @@ import database from "./data.json";
 
 export default function List() {
     return (
-        <section className="my-5">
-            <h1 className={"heading-1 text-3xl mx-3 lg:mx-10"}>Liste des entreprises</h1>
-            <div className={"flex flex-col my-4 gap-4 lg:flex-wrap lg:flex-row lg:justify-center align-center"}>
+        <section className="my-5 mx-2">
+            <h1 className="heading-1 text-3xl mx-3 lg:mx-10">Liste des entreprises</h1>
+            <div className="flex flex-col my-4 gap-4 lg:flex-wrap lg:flex-row lg:justify-center align-center">
                 
             {
                 database.data.map((obj) => (
